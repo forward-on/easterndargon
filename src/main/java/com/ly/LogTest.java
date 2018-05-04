0350b72e40a07157396d2dfe960086d73b98c407
liying <liying@ming800.com>
2018-05-04 18:21:13 +0800

    first commit

---
+++ src/main/java/com/ly/LogTest.java
@@ -0,0 +1,31 @@
+package com.ly;
+
+
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+import java.io.File;
+
+/**
+ * @author : ly.
+ * @Date : 2018/5/4.
+ */
+@Controller
+public class LogTest {
+
+    private static Logger logger = LoggerFactory.getLogger(LogTest.class);
+
+    public static void main(String[] args) {
+        String path = Class.class.getClass().getResource("/").getPath();
+        System.out.println(path);
+        PropertyConfigurator.configure(path + "/log4j.properties");
+//        Logger logger = LoggerFactory.getLogger("parkLogger");
+        logger.debug("debug---");
+        logger.info("info---");
+        logger.error("error--");
+        logger.warn("warn---");
+    }
+
+}
