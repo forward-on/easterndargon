70280c4bb779848909d9b7c7fb1efd6b8f86e084
liying <liying@ming800.com>
2018-05-14 23:23:57 +0800

    map

---
+++ src/main/java/com/ly/contextanalyze/MapAnalyse.java
@@ -0,0 +1,20 @@
+package com.ly.contextanalyze;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MapAnalyse {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+
+
+    }
+
+    public void concurrentHashMap(){
+        ConcurrentHashMap<Object, Object> concurrentHashMap = new ConcurrentHashMap<>();
+
+    }
+
+}
